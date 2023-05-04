de7195da941f5cd5acb4a2031d28a4dadc53672d
Marius Lupasco <mariuslupasco16@gmail.com>
2023-05-04 21:58:03 +0300

    #?? - Added Hub methods for handling Offer/Answer operations

--- RtcMeetingSignal.App/Hubs/SignalingHub.cs
+++ RtcMeetingSignal.App/Hubs/SignalingHub.cs
@@ -4,8 +4,12 @@ namespace RtcMeetingSignal.App.Hubs;
 
 public class SignalingHub : Hub
 {
+    private const string ReceiveMessage = nameof(ReceiveMessage);
+    private const string ReceiveOffer = nameof(ReceiveOffer);
+    private const string ReceiveAnswer = nameof(ReceiveAnswer);
+    
     private static List<string> _connections = new();
-
+        
     public override Task OnConnectedAsync()
     {
         var connectionId = Context.ConnectionId;
@@ -14,14 +18,26 @@ public class SignalingHub : Hub
         return base.OnConnectedAsync();
     }
 
-    public async Task SendMessage(string message)
+    public async Task SendMessageAsync(string message)
     {
         foreach (var connection in _connections)
         {
             if (Context.ConnectionId != connection)
             {
-                await Clients.Client(connection).SendAsync("ReceiveMessage", message);
+                await Clients.Client(connection).SendAsync(ReceiveMessage, message);
             }
         }
     }
+
+    // TODO Decide on offer structure
+    public async Task SendOfferAsync(object offer, string connectionId)
+    {
+        await Clients.Client(connectionId).SendAsync(ReceiveOffer, offer);
+    }
+
+    public async Task SendAnswerAsync(object answer, string connectionId)
+    {
+        await Clients.Client(connectionId).SendAsync(ReceiveAnswer);
+    }
+    
 }
